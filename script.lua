60d52411b99e69f0b7ce3732ab621e2d3aadc1f5
NormalDudeBro <68166174+NormalDudeBro@users.noreply.github.com>
2021-12-20 21:36:51 -0800

    please work
    hfadfdasga

--- script.lua
+++ script.lua
@@ -53,11 +53,11 @@ getgenv().DontTeleportTheSameNumber = true --If you set this true it won't telep
 getgenv().CopytoClipboard = true
 
 if not game:IsLoaded() then
-  print("Game is loading. Waiting...")
-  repeat
-    wait()
-  until game:IsLoaded()
-end
+    print("Game is loading waiting... | Empty Server Finder")
+    repeat
+        wait()
+    until game:IsLoaded()
+    end
 
 local maxplayers = math.huge
 local serversmaxplayer;
@@ -65,47 +65,47 @@ local goodserver;
 local gamelink = "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100"
 
 function serversearch()
-  for _, v in pairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync(gamelink)).data) do
-    if type(v) == "table" and maxplayers > v.playing then
-      serversmaxplayer = v.maxPlayers
-      maxplayers = v.playing
-      goodserver = v.id
+    for _, v in pairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync(gamelink)).data) do
+        if type(v) == "table" and maxplayers > v.playing then
+            serversmaxplayer = v.maxPlayers
+            maxplayers = v.playing
+            goodserver = v.id
+        end
     end
-  end
-  print("Currently checking the servers with max this number of players : " .. maxplayers .. "")
+    print("Currently checking the servers with max this number of players : " .. maxplayers .. " | Empty Server Finder")
 end
 
 function getservers()
-  serversearch()
-  for i,v in pairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync(gamelink))) do
-    if i == "nextPageCursor" then
-      if gamelink:find("&cursor=") then
-        local a = gamelink:find("&cursor=")
-        local b = gamelink:sub(a)
-        gamelink = gamelink:gsub(b, "")
-      end
-      gamelink = gamelink .. "&cursor=" ..v
-      getservers()
+    serversearch()
+    for i,v in pairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync(gamelink))) do
+        if i == "nextPageCursor" then
+            if gamelink:find("&cursor=") then
+                local a = gamelink:find("&cursor=")
+                local b = gamelink:sub(a)
+                gamelink = gamelink:gsub(b, "")
+            end
+            gamelink = gamelink .. "&cursor=" ..v
+            getservers()
+        end
     end
-  end
 end
 
 getservers()
 
-print("All of the servers are searched")
-print("Server : " .. goodserver .. " Players : " .. maxplayers .. "/" .. serversmaxplayer .. "")
-if CopytoClipboard then
-  setclipboard(goodserver)
-end
-if AutoTeleport then
-  if DontTeleportTheSameNumber then
-    if #game:GetService("Players"):GetPlayers() - 1 == maxplayers then
-      return warn("It has same number of players (except you)")
-    elseif goodserver == game.JobId then
-      return warn("Your current server is the most empty server atm")
+    print("All of the servers are searched") 
+	print("Server : " .. goodserver .. " Players : " .. maxplayers .. "/" .. serversmaxplayer .. " | Empty Server Finder")
+    if CopytoClipboard then
+    setclipboard(goodserver)
     end
-  end
-  print("AutoTeleport is enabled. Teleporting to : " .. goodserver)
-  game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, goodserver)
-end
+    if AutoTeleport then
+        if DontTeleportTheSameNumber then 
+            if #game:GetService("Players"):GetPlayers() - 1 == maxplayers then
+                return warn("It has same number of players (except you)")
+            elseif goodserver == game.JobId then
+                return warn("Your current server is the most empty server atm") 
+            end
+        end
+        print("AutoTeleport is enabled. Teleporting to : " .. goodserver)
+        game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, goodserver)
+    end
 end)
