06cd79a1baa99e98d2e9523955641acdaa30e769
NormalDudeBro <68166174+NormalDudeBro@users.noreply.github.com>
2021-12-20 21:30:48 -0800

    fixed the last commit
    my small brain did math.inf and not math.huge

--- script.lua
+++ script.lua
@@ -59,7 +59,7 @@ if not game:IsLoaded() then
   until game:IsLoaded()
 end
 
-local maxplayers = math.inf
+local maxplayers = math.huge
 local serversmaxplayer;
 local goodserver;
 local gamelink = "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100"
