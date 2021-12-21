2d733cbf3e82a2054d3d5a4db14665f7054de90e
NormalDudeBro <68166174+NormalDudeBro@users.noreply.github.com>
2021-12-20 21:19:31 -0800

    added sever hop
    i added server hop

--- script.lua
+++ script.lua
@@ -2,7 +2,7 @@ local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kiwi-
 library.options.underlinecolor = "rainbow"
 local w = library:CreateWindow('Main')
 w:Section('The Goods')
-local Toggle = w:Toggle("Autokill Toggle", {flag = "TheYes"})
+local autokilltoggle = w:Toggle("Autokill Toggle", {flag = "TheYes"})
 spawn(
 function()
   while wait() do
@@ -34,7 +34,7 @@ function()
   end
 end
 )
-local Toggle = w:Toggle("Anti AFK toggle", {flag = "TheAFK"})
+local afktoggle = w:Toggle("Anti AFK toggle", {flag = "TheAFK"})
 spawn(
 function()
   while wait() do
@@ -47,3 +47,65 @@ function()
   end
 end
 )
+local serverhop  = w:Button("Click me to join the smallest server!", function() --I DIDNT MAKE THIS EITHER CREDIT TO https://v3rmillion.net/showthread.php?tid=1107863
+getgenv().AutoTeleport = true
+getgenv().DontTeleportTheSameNumber = true --If you set this true it won't teleport to the server if it has the same number of players as your current server
+getgenv().CopytoClipboard = true
+
+if not game:IsLoaded() then
+  print("Game is loading. Waiting...")
+  repeat
+    wait()
+  until game:IsLoaded()
+end
+
+local maxplayers = 8
+local serversmaxplayer;
+local goodserver;
+local gamelink = "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100"
+
+function serversearch()
+  for _, v in pairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync(gamelink)).data) do
+    if type(v) == "table" and maxplayers > v.playing then
+      serversmaxplayer = v.maxPlayers
+      maxplayers = v.playing
+      goodserver = v.id
+    end
+  end
+  print("Currently checking the servers with max this number of players : " .. maxplayers .. "")
+end
+
+function getservers()
+  serversearch()
+  for i,v in pairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync(gamelink))) do
+    if i == "nextPageCursor" then
+      if gamelink:find("&cursor=") then
+        local a = gamelink:find("&cursor=")
+        local b = gamelink:sub(a)
+        gamelink = gamelink:gsub(b, "")
+      end
+      gamelink = gamelink .. "&cursor=" ..v
+      getservers()
+    end
+  end
+end
+
+getservers()
+
+print("All of the servers are searched")
+print("Server : " .. goodserver .. " Players : " .. maxplayers .. "/" .. serversmaxplayer .. "")
+if CopytoClipboard then
+  setclipboard(goodserver)
+end
+if AutoTeleport then
+  if DontTeleportTheSameNumber then
+    if #game:GetService("Players"):GetPlayers() - 1 == maxplayers then
+      return warn("It has same number of players (except you)")
+    elseif goodserver == game.JobId then
+      return warn("Your current server is the most empty server atm")
+    end
+  end
+  print("AutoTeleport is enabled. Teleporting to : " .. goodserver)
+  game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, goodserver)
+end
+end)
